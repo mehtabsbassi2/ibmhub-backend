1d75434ed5947ac7ee62c436effe29b126c4fd5e
mehtabsbassi2 <148270493+mehtabsbassi2@users.noreply.github.com>
2025-09-19 17:23:35 +0300

    Update UserSkill.js

--- api/models/UserSkill.js
+++ api/models/UserSkill.js
@@ -7,8 +7,10 @@ const UserSkill = sequelize.define("UserSkill", {
   level: { type: DataTypes.INTEGER, defaultValue: 0 },
   questions_answered: { type: DataTypes.INTEGER, defaultValue: 0 },
   votes_recieved: { type: DataTypes.INTEGER, defaultValue: 0 },
-  authorId: { type: DataTypes.STRING, allowNull: false } 
+  authorId: { type: DataTypes.STRING, allowNull: false },
+   targetRoleId: { type: DataTypes.INTEGER, allowNull: true },
 });
 
 
 module.exports = UserSkill;
+
