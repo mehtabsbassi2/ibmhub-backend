a3e069803d2c3aaa677142c322f5e17885d0a802
mehtabsbassi2 <148270493+mehtabsbassi2@users.noreply.github.com>
2025-10-20 19:35:19 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -19,7 +19,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ["https://ibmhub-client.vercel.app"],
+  methods: ["GET","POST","PUT","DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Routes
@@ -56,6 +61,7 @@ sequelize.authenticate()
 //   .catch((err) => console.error("❌ Database connection failed:", err));
 
 // module.exports.handler = serverless(app);
+
 
 
 
