dbd4330881bce26a8b5312e4033e2c37faff7478
mehtabsbassi2 <148270493+mehtabsbassi2@users.noreply.github.com>
2025-12-05 11:39:03 +0300

    Delete api/models/Tag.js

--- api/models/Tag.js
+++
@@ -1,9 +0,0 @@
-// tag.js
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db');
-
-const Tag = sequelize.define('Tag', {
-  name: { type: DataTypes.STRING, unique: true }
-});
-
-module.exports = Tag;
