f4fea41e54713078b10fdc048a0cb0dc2e957bb4
mehtabsbassi2 <148270493+mehtabsbassi2@users.noreply.github.com>
2025-12-05 11:33:37 +0300

    Delete api/models/QuestionTag.js

--- api/models/QuestionTag.js
+++
@@ -1,9 +0,0 @@
-// questionTag.js
-const sequelize = require('../config/db');
-
-
-const QuestionTag = sequelize.define('QuestionTag', {}, { timestamps: false });
-
-
-
-module.exports = QuestionTag;
